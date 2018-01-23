de46ad2dd1392199006daa02d99aab776cd43afd
winjeysong <winjey-song@163.com>
2018-01-23 14:21:06 +0800

    use Request.params

--- app3.js
+++ app3.js
@@ -34,34 +34,34 @@ app.get('/maomao', (request, response) => {
   response.redirect('/index.html');
 });
 
-app.all('/user/*?', (req, res, next) => { // general config for route: /user/...
+app.all('/:objType/*?', (req, res, next) => { // general config for route: /user/...
   res.contentType('json');
   next();
 });
 
-app.get('/user/list', (req, res) => { // get user list
-  res.send({ title: 'user list' });
+app.get('/:objType/list', (req, res) => { // get user list
+  res.send({ title: `${req.params.objType} list` });
 });
 
-app.post('/user/create', (req, res) => { // create user
-  res.send({ title: 'user created' });
+app.post('/:objType/create', (req, res) => { // create user
+  res.send({ title: `${req.params.objType} created` });
 });
 
-app.get('/user/read/:id(\\d+)', (req, res) => { // find user & its ID
+app.get('/:objType/read/:id(\\d+)', (req, res) => { // find user & its ID
   res.send({
-    title: `user with id ${req.params.id} found`,
+    title: `${req.params.objType} with id ${req.params.id} found`,
   });
 });
 
-app.get('/user/delete/:id(\\d+)', (req, res) => { // delete user
+app.get('/:objType/delete/:id(\\d+)', (req, res) => { // delete user
   res.send({
-    title: `user with id ${req.params.id} deleted`,
+    title: `${req.params.objType} with id ${req.params.id} deleted`,
   });
 });
 
-app.get('/user/update/:id(\\d+)', (req, res) => { // update user
+app.get('/:objType/update/:id(\\d+)', (req, res) => { // update user
   res.send({
-    title: `user with id ${req.params.id} updated`,
+    title: `${req.params.objType} with id ${req.params.id} updated`,
   });
 });
 /* --------- server config ---------- */
