4ec64cc84d51f6485fe5fcde064d0217cb4a5798
winjeysong <winjey-song@163.com>
2018-01-23 14:36:09 +0800

    add routes module

--- app3.js
+++ app3.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const errorhandler = require('errorhandler');
+const myRoutes = require('./routes');
 
 // variables
 const app = express();
@@ -30,40 +31,7 @@ if (app.get('env') === 'production') {
   app.use(errorhandler());
 }
 // routes
-app.get('/maomao', (request, response) => {
-  response.redirect('/index.html');
-});
-
-app.all('/:objType/*?', (req, res, next) => { // general config for route: /user/...
-  res.contentType('json');
-  next();
-});
-
-app.get('/:objType/list', (req, res) => { // get user list
-  res.send({ title: `${req.params.objType} list` });
-});
-
-app.post('/:objType/create', (req, res) => { // create user
-  res.send({ title: `${req.params.objType} created` });
-});
-
-app.get('/:objType/read/:id(\\d+)', (req, res) => { // find user & its ID
-  res.send({
-    title: `${req.params.objType} with id ${req.params.id} found`,
-  });
-});
-
-app.get('/:objType/delete/:id(\\d+)', (req, res) => { // delete user
-  res.send({
-    title: `${req.params.objType} with id ${req.params.id} deleted`,
-  });
-});
-
-app.get('/:objType/update/:id(\\d+)', (req, res) => { // update user
-  res.send({
-    title: `${req.params.objType} with id ${req.params.id} updated`,
-  });
-});
+myRoutes.configRoutes(app);
 /* --------- server config ---------- */
 
 // server start
